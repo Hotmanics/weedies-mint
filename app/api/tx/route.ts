0a870e8e634d716a754433d0dbe2cfcacf2f1fb2
Jacob Homanics <homanicsjake@gmail.com>
2024-04-16 23:41:29 -0500

    Update route.ts

--- app/api/tx/route.ts
+++ app/api/tx/route.ts
@@ -1,7 +1,8 @@
 import { FrameRequest, getFrameMessage } from '@coinbase/onchainkit/frame';
 import { NextRequest, NextResponse } from 'next/server';
 import { encodeFunctionData, parseEther } from 'viem';
-import { base } from 'viem/chains';
+import { sepolia, base } from 'viem/chains';
+
 import BuyMeACoffeeABI from '../../_contracts/BuyMeACoffeeABI';
 import { BUY_MY_COFFEE_CONTRACT_ADDR } from '../../config';
 import type { FrameTransactionResponse } from '@coinbase/onchainkit/frame';
@@ -21,13 +22,13 @@ async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
   });
 
   const txData: FrameTransactionResponse = {
-    chainId: `eip155:${base.id}`, // Remember Base Sepolia might not work on Warpcast yet
+    chainId: `eip155:${sepolia.id}`, // Remember Base Sepolia might not work on Warpcast yet
     method: 'eth_sendTransaction',
     params: {
       abi: [],
       data,
       to: BUY_MY_COFFEE_CONTRACT_ADDR,
-      value: parseEther('0.00004').toString(), // 0.00004 ETH
+      value: parseEther('0.00001').toString(), // 0.00004 ETH
     },
   };
   return NextResponse.json(txData);
